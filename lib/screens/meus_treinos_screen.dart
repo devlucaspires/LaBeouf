619324d373dedd5c4fd6643d209d6ce9cce854ad
devlucaspires <lucaspires.dev@gmail.com>
2026-09-01 11:41:49 -0300

    feat: adicionar stubs das telas meus_treinos e montar_treino

---
+++ lib/screens/meus_treinos_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MeusTreinosScreen extends StatelessWidget {
+  const MeusTreinosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Meus Treinos')),
+      body: Center(child: Text('Em breve...')),
+    );
+  }
+}
---
+++ lib/screens/montar_treino_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MontarTreinoScreen extends StatelessWidget {
+  const MontarTreinoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Montar Treino')),
+      body: Center(child: Text('Em breve...')),
+    );
+  }
+}
